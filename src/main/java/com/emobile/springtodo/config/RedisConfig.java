3a3b0ad832d910f20a3f7c70afe767357642d2f9
nick-long-int <gnidenkonn@yandex.ru>
2026-04-18 16:16:28 +0300

    Добавление сериализации значений для редиса

--- src/main/java/com/emobile/springtodo/config/RedisConfig.java
+++ src/main/java/com/emobile/springtodo/config/RedisConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -16,7 +18,10 @@ public class RedisConfig {
         return RedisCacheManager.builder(redisConnectionFactory)
             .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(5))
-                .disableCachingNullValues())
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext
+                    .SerializationPair
+                    .fromSerializer(new GenericJackson2JsonRedisSerializer())))
             .transactionAware()
             .build();
     }
